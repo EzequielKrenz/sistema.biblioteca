c10831c516e3a03d9b711c5e95b26debe3ebf1f5
EzequielKrenz <ezequielkrenz060@gmail.com>
2025-09-11 22:05:21 -0300

    comit inicial

--- sistema-gerenciamento-biblioteca/sgb_livros/urls.py
+++ sistema-gerenciamento-biblioteca/sgb_livros/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.livros),
-    path("salva_livro" , views.salva_livro)
+    #path('', views.livros),
+    #path("salva_livro" , views.salva_livro)
+    path('', views.cadastra_livro, name='cadastra_livro'),
 ]
--- sistema-gerenciamento-biblioteca/sgb_livros/views.py
+++ sistema-gerenciamento-biblioteca/sgb_livros/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Livro
 
 # Create your views here.
 def livros(request):
@@ -10,3 +11,34 @@ def salva_livro(request):
     autor_livro = request.POST["autor_livro"]
     editora_livro = request.POST["editora_livro"]
     return render(request, 'livros.html', context={'titulo_livro':titulo_livro})
+
+def cadastra_livro(request):
+    if request.method == 'POST':
+        livro_id = request.POST['livro_id']
+        titulo = request.POST['titulo']
+        autor = request.POST['autor']
+        ano_publicacao = request.POST['ano_publicacao']
+        editora = request.POST['editora']
+
+        if livro_id: #Edita Livro
+            livro = livro_id
+            livro.titulo = titulo
+            livro.autor = autor
+            livro.ano_publicacao = ano_publicacao
+            livro.editora = editora
+            livro.save()
+        else: #salva um novo livro
+            Livro.objects.create(
+                titulo = titulo,
+                autor = autor,
+                ano_publicacao = ano_publicacao,
+                editora = editora
+            )
+
+        return redirect('cadastra_livro')
+    # objects é o gerenciamento do Django que serveparaconsultar no banco
+    #all() é um comando que retorna todos os cadastros da tabela livro
+    livros = Livro.objects.all()
+    return render(request, 'livros.html', {'livros': livros})
+
+       
