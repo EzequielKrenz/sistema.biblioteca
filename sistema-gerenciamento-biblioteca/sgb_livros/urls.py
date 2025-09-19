a92c3b86332108ec0ca6f47e270d523564f6c2b4
EzequielKrenz <ezequielkrenz060@gmail.com>
2025-09-18 21:11:47 -0300

    aula 18/09 - python form sql(editar e excluir)

--- sistema-gerenciamento-biblioteca/sgb_livros/urls.py
+++ sistema-gerenciamento-biblioteca/sgb_livros/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     #path('', views.livros),
     #path("salva_livro" , views.salva_livro)
     path('', views.cadastra_livro, name='cadastra_livro'),
+    path('excluir/<int:livro_id>', views.exclui_livro, name='exclui_livro'),
+    path('editar/<int:livro_id>', views.edita_livro, name='edita_livro'),
+
 ]
