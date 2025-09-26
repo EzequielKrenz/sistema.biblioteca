c5a8047ebae64b2ef79d38ab57da0fb44ce40479
EzquielKrenz <ezequielkrenz060@gmail.com>
2025-09-25 22:30:34 -0300

    COMIT AULA 10

--- sistema-gerenciamento-biblioteca/sgb/urls.py
+++ sistema-gerenciamento-biblioteca/sgb/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('livros/', include('sgb_livros.urls')),
-
+    path('auth/', include('sgb_usuarios.urls'))
 ]
--- sistema-gerenciamento-biblioteca/sgb_livros/urls.py
+++ sistema-gerenciamento-biblioteca/sgb_livros/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     #path("salva_livro" , views.salva_livro)
     path('', views.cadastra_livro, name='cadastra_livro'),
     path('excluir/<int:livro_id>', views.exclui_livro, name='exclui_livro'),
-    path('editar/<int:livro_id>', views.edita_livro, name='edita_livro'),
-
+    path('editar/<int:livro_id>', views.edita_livro, name='edita_livro')
 ]
